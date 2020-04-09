cd9b27bc472a476c3d416c1069f622aac3d12831
Alex <almartin86@yahoo.fr>
2020-04-09 11:58:59 +0200

    env

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ const app = require("./app");
 const PORT = process.env.PORT || 3000;
 
 const mongoose = require("mongoose");
-const DB_USER = "gltrunky";
-const DB_PWD = "MC1KDi1JpSVjQpfY";
-const DB_HOST = "cluster0-kkcso.mongodb.net";
-const DB_NAME = "social_network";
+const { DB_NAME, DB_HOST, DB_PWD, DB_USER } = process.env;
 const dbUrl = `mongodb+srv://${DB_USER}:${DB_PWD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
 
 //Connexion a la BDD
@@ -22,3 +19,14 @@ mongoose.connect(
 );
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
+
+// TEST DE LIASON AVEC DB MOOGOSE
+
+// const Schema = mongoose.Schema;
+// const UserSchema = Schema({
+//   name: String,
+//   age: Number,
+// });
+
+// const User = mongoose.model("User", UserSchema);
+// User.findById("5e8ee93b1c9d440000c58394").then((user) => console.log(user));
